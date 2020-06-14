39971c3cf91efb124f009e0757ae7339e8374476
Linus <linus.hafkemeyer@protonmail.ch>
2020-06-14 12:53:52 +0200

    Add boost test skeleton

---
+++ plugins/sep/test/slipresentation-tests.cc
@@ -0,0 +1,14 @@
+#include "../sli/slipresentation.hh"
+
+#include <boost/test/unit_test.hpp>
+
+
+BOOST_AUTO_TEST_SUITE(Sli_Tests)
+
+BOOST_AUTO_TEST_CASE(dummy)
+{
+  bool x = true;
+  BOOST_REQUIRE(x);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
