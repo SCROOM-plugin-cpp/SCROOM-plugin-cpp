4b5a576045b6599f7c981b24418c27cd66d545f4
LukeSerne <l.j.serne@student.tue.nl>
2020-06-22 10:24:17 +0200

    First attempt at filltiles test

--- plugins/sep/test/sepsource-test.cc
+++ plugins/sep/test/sepsource-test.cc
@@ -1,5 +1,7 @@
 #include <boost/dll.hpp>
 #include <boost/test/unit_test.hpp>
+#include "scroom/blockallocator.hh"
+#include "scroom/tiledbitmaplayer.hh"
 
 // Make all private members accessible for testing
 #define private public
@@ -228,6 +230,27 @@ BOOST_AUTO_TEST_CASE(fill_sli_1) {
     BOOST_CHECK(sli->bitmap != nullptr);
     BOOST_CHECK(std::abs(sli->xAspect - 1.0) < 1e-4);
     BOOST_CHECK(std::abs(sli->yAspect - 1.0) < 1e-4);
+}
+
+BOOST_AUTO_TEST_CASE(fill_tiles) {
+    // Preparation
+    auto source = SepSource::create();
+    SepFile file = SepSource::parseSep((testFileDir / "sep_cmyk.sep").string());
+    source->setData(file);
+    source->openFiles();
+
+    auto blockiface = Scroom::MemoryBlocks::getBlockFactoryInterface()->create(1, TILESIZE);
+    auto provider = blockiface->getPages().front().get();
+    auto alloc = std::allocator<Tile::Ptr>();
+    auto tiles = std::vector<Tile::Ptr>(alloc);
+    tiles.push_back(Tile::create(TILESIZE, TILESIZE, 8, provider));
+
+    // Tested call
+    // Currently segfaults due to the weird tiles setup.
+    // source->fillTiles(0, file.height, TILESIZE, 0, tiles);
+
+    // Checks
+    BOOST_CHECK(true);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
