9afa4b44e07a05b3034cae12c18fe36139904c5a
Roan Hofland <roan.hofland@hotmail.com>
2020-06-23 14:25:36 +0200

    Remove copyright header

--- plugins/pipette/src/pipette.hh
+++ plugins/pipette/src/pipette.hh
@@ -1,10 +1,3 @@
-/*
- * Scroom - Generic viewer for 2D data
- * Copyright (C) 2009-2020 Kees-Jan Dijkzeul
- *
- * SPDX-License-Identifier: LGPL-2.1
- */
-
 #pragma once
 
 #include <scroom/plugininformationinterface.hh>
