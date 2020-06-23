0e995ecee730c55d30972c9278910225e62a66ea
noobieprogram <a.saeed@student.tue.nl>
2020-06-23 20:37:43 +0200

    added some more test cases for seppresentation

--- plugins/sep/test/seppresentation-test.cc
+++ plugins/sep/test/seppresentation-test.cc
@@ -5,6 +5,7 @@
 // Make all private members accessible for testing
 #define private public
 
+#include "../sep.hh"
 #include "../seppresentation.hh"
 #include "../sepsource.hh"
 
@@ -17,14 +18,21 @@ BOOST_AUTO_TEST_CASE(create) {
     BOOST_CHECK(presentation != nullptr);
 }
 
-BOOST_AUTO_TEST_CASE(load_1) {
+BOOST_AUTO_TEST_CASE(load_false) {
     SepPresentation::Ptr presentation = SepPresentation::create();
     BOOST_CHECK_EQUAL(presentation->load((testFileDir / "sep_test.sep").string()), false);
+    BOOST_CHECK_EQUAL(presentation->width, 0);
+    BOOST_CHECK_EQUAL(presentation->height, 0);
 }
 
-BOOST_AUTO_TEST_CASE(load_2) {
+BOOST_AUTO_TEST_CASE(load_true) {
     SepPresentation::Ptr presentation = SepPresentation::create();
     BOOST_CHECK_EQUAL(presentation->load((testFileDir / "sep_cmyk.sep").string()), true);
+    BOOST_CHECK_EQUAL(presentation->width, 600);
+    BOOST_CHECK_EQUAL(presentation->height, 400);
+    for (auto c : presentation->sep_source->channels) {
+        BOOST_CHECK(presentation->sep_source->channel_files[c] != nullptr);
+    }
 }
 
 BOOST_AUTO_TEST_CASE(getTitle) {
@@ -49,5 +57,29 @@ BOOST_AUTO_TEST_CASE(getRect) {
     BOOST_CHECK(rect.getHeight() == 400);
 }
 
+BOOST_AUTO_TEST_CASE(getViews) {
+    SepPresentation::Ptr presentation = SepPresentation::create();
+    presentation->load((testFileDir / "sep_cmyk.sep").string());
+    auto views = presentation->getViews();
+    BOOST_CHECK(views.empty());
+}
+
+BOOST_AUTO_TEST_CASE(pipette) {
+    SepPresentation::Ptr presentation = SepPresentation::create();
+    BOOST_CHECK_EQUAL(presentation->load((testFileDir / "sep_cmyk.sep").string()), true);
+    Scroom::Utils::Rectangle<int> rect = presentation->getRect();
+    BOOST_CHECK(rect.getTop() == 0);
+    BOOST_CHECK(rect.getLeft() == 0);
+    BOOST_CHECK(rect.getWidth() == 600);
+    BOOST_CHECK(rect.getHeight() == 400);
+
+    for (auto c : presentation->sep_source->channels) {
+        BOOST_CHECK(presentation->sep_source->channel_files[c] != nullptr);
+    }
+
+    auto averages = presentation->getPixelAverages(rect);
+
+    BOOST_CHECK(averages != nullptr);
+}
 
 BOOST_AUTO_TEST_SUITE_END()
