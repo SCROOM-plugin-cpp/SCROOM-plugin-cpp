9e7ae644d14bf0829ec184ecf7aa657f25e8ff87
Marco <m.palazzo@student.tue.nl>
2020-06-30 09:15:28 -0700

    fixed missing files and duplicate functions

--- plugins/sep/test/slipresentation-tests.cc
+++ plugins/sep/test/slipresentation-tests.cc
@@ -7,13 +7,46 @@
 
 #include "../sli/slipresentation.hh"
 #include <scroom/scroominterface.hh>
-#include "sli_test_helper_functions.hh"
 
 #define SLI_NOF_LAYERS 4
 
 const std::string testFileDir =
     boost::dll::program_location().parent_path().parent_path().string() +
     "/testfiles/";
+
+///////////////////////////////////////////////////////////////////////////////
+// Helper functions
+
+void dummyFunc(){};
+
+SliPresentation::Ptr createPresentation() {
+  SliPresentation::Ptr presentation = SliPresentation::create(nullptr);
+  BOOST_REQUIRE(presentation);
+  // Assign the callbacks to dummy functions to avoid exceptions
+  presentation->source->enableInteractions = boost::bind(dummyFunc);
+  presentation->source->disableInteractions = boost::bind(dummyFunc);
+
+  return presentation;
+}
+
+void dummyRedraw(SliPresentation::Ptr presentation) {
+  // Create dummy objects to call redraw() with
+  cairo_surface_t *surface =
+      cairo_image_surface_create(CAIRO_FORMAT_ARGB32, 100, 100);
+  cairo_t *cr = cairo_create(surface);
+  Scroom::Utils::Rectangle<double> rect(0.0, 0.0, 100.0, 100.0);
+
+  boost::this_thread::sleep(boost::posix_time::millisec(1000));
+  // redraw() for all zoom levels from 5 to -2 and check whether cache has been
+  // computed
+  for (int zoom = 5; zoom > -3; zoom--) {
+    presentation->redraw(nullptr, cr, rect, zoom);
+    boost::this_thread::sleep(boost::posix_time::millisec(
+        1000)); // Very liberal, shouldn't fail beause of time
+    BOOST_REQUIRE(presentation->source->rgbCache[std::min(0, zoom)]);
+  }
+  BOOST_REQUIRE(presentation);
+}
 
 ///////////////////////////////////////////////////////////////////////////////
 // Tests
--- plugins/sep/test/slisource-tests.cc
+++ plugins/sep/test/slisource-tests.cc
@@ -7,7 +7,6 @@
 
 #include "../sli/slipresentation.hh"
 #include <scroom/scroominterface.hh>
-#include "sli_test_helper_functions.hh"
 
 #define SLI_NOF_LAYERS 4
 
@@ -16,15 +15,49 @@ const std::string testFileDir =
     "/testfiles/";
 
 ///////////////////////////////////////////////////////////////////////////////
+// Helper functions
+
+void dummyFunc1(){};
+
+SliPresentation::Ptr createPresentation1() {
+  SliPresentation::Ptr presentation = SliPresentation::create(nullptr);
+  BOOST_REQUIRE(presentation);
+  // Assign the callbacks to dummy functions to avoid exceptions
+  presentation->source->enableInteractions = boost::bind(dummyFunc1);
+  presentation->source->disableInteractions = boost::bind(dummyFunc1);
+
+  return presentation;
+}
+
+void dummyRedraw1(SliPresentation::Ptr presentation) {
+  // Create dummy objects to call redraw() with
+  cairo_surface_t *surface =
+      cairo_image_surface_create(CAIRO_FORMAT_ARGB32, 100, 100);
+  cairo_t *cr = cairo_create(surface);
+  Scroom::Utils::Rectangle<double> rect(0.0, 0.0, 100.0, 100.0);
+
+  boost::this_thread::sleep(boost::posix_time::millisec(1000));
+  // redraw() for all zoom levels from 5 to -2 and check whether cache has been
+  // computed
+  for (int zoom = 5; zoom > -3; zoom--) {
+    presentation->redraw(nullptr, cr, rect, zoom);
+    boost::this_thread::sleep(boost::posix_time::millisec(
+        1000)); // Very liberal, shouldn't fail beause of time
+    BOOST_REQUIRE(presentation->source->rgbCache[std::min(0, zoom)]);
+  }
+  BOOST_REQUIRE(presentation);
+}
+
+///////////////////////////////////////////////////////////////////////////////
 // Tests
 
 BOOST_AUTO_TEST_SUITE(Sli_Tests)
 
 BOOST_AUTO_TEST_CASE(slisource_clearbottomsurface_all_toggled) {
-  SliPresentation::Ptr presentation = createPresentation();
+  SliPresentation::Ptr presentation = createPresentation1();
 
   presentation->load(testFileDir + "sli_tiffonly.sli");
-  dummyRedraw(presentation);
+  dummyRedraw1(presentation);
   presentation->source->toggled = boost::dynamic_bitset<> {SLI_NOF_LAYERS}.set();
   presentation->source->clearBottomSurface();
   int total_height = presentation->source->total_height;
@@ -43,13 +76,13 @@ BOOST_AUTO_TEST_CASE(slisource_clearbottomsurface_all_toggled) {
 }
 
 BOOST_AUTO_TEST_CASE(slisource_clearbottomsurface_none_toggled) {
-  SliPresentation::Ptr presentation1 = createPresentation();
-  SliPresentation::Ptr presentation2 = createPresentation();
+  SliPresentation::Ptr presentation1 = createPresentation1();
+  SliPresentation::Ptr presentation2 = createPresentation1();
 
   presentation1->load(testFileDir + "sli_tiffonly.sli");
   presentation2->load(testFileDir + "sli_tiffonly.sli");
-  dummyRedraw(presentation1);
-  dummyRedraw(presentation2);
+  dummyRedraw1(presentation1);
+  dummyRedraw1(presentation2);
   presentation1->source->toggled = boost::dynamic_bitset<> {SLI_NOF_LAYERS};
   presentation1->source->clearBottomSurface();
   int total_height = presentation1->source->total_height;
@@ -69,13 +102,13 @@ BOOST_AUTO_TEST_CASE(slisource_clearbottomsurface_none_toggled) {
 }
 
 BOOST_AUTO_TEST_CASE(slisource_clearbottomsurface_some_toggled) {
-  SliPresentation::Ptr presentation1 = createPresentation();
-  SliPresentation::Ptr presentation2 = createPresentation();
+  SliPresentation::Ptr presentation1 = createPresentation1();
+  SliPresentation::Ptr presentation2 = createPresentation1();
 
   presentation1->load(testFileDir + "sli_tiffonly.sli");
   presentation2->load(testFileDir + "sli_tiffonly.sli");
-  dummyRedraw(presentation1);
-  dummyRedraw(presentation2);
+  dummyRedraw1(presentation1);
+  dummyRedraw1(presentation2);
   presentation1->source->toggled = boost::dynamic_bitset<> {SLI_NOF_LAYERS}.set(0);
   presentation1->source->clearBottomSurface();
   int height = presentation1->source->layers[0]->height;
@@ -112,9 +145,9 @@ BOOST_AUTO_TEST_CASE(slisource_clearbottomsurface_some_toggled) {
 // tinycmyk.tif (bgra) = 
 //              [(255,255,0,255),(255,0,255,255),(0,255,255,255),(0,0,0,255)]
 BOOST_AUTO_TEST_CASE(slisource_computergb_yoffset) {
-  SliPresentation::Ptr presentation = createPresentation();
+  SliPresentation::Ptr presentation = createPresentation1();
   presentation->load(testFileDir + "sli_tinycmyk.sli");
-  dummyRedraw(presentation);
+  dummyRedraw1(presentation);
   auto surface = presentation->source->getSurface(0)->getBitmap();
   presentation->source->computeRgb();
   // bgra conversion of tinycmyk.tif
@@ -126,9 +159,9 @@ BOOST_AUTO_TEST_CASE(slisource_computergb_yoffset) {
 }
 
 BOOST_AUTO_TEST_CASE(slisource_computergb_xoffset) {
-  SliPresentation::Ptr presentation = createPresentation();
+  SliPresentation::Ptr presentation = createPresentation1();
   presentation->load(testFileDir + "sli_tinycmyk_xoffset.sli");
-  dummyRedraw(presentation);
+  dummyRedraw1(presentation);
   auto surface = presentation->source->rgbCache[0]->getBitmap();
   presentation->source->computeRgb();
   // bgra conversion of tinycmyk.tif
