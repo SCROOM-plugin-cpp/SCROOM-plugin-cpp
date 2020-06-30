afc4d7f5854cd25d09085da0246c5521c3609eca
Marco <m.palazzo@student.tue.nl>
2020-06-30 09:38:02 -0700

    added sep tests from PR #13

--- plugins/sep/test/sep-tests.cc
+++ plugins/sep/test/sep-tests.cc
@@ -2,7 +2,6 @@
 
 #include <boost/dll.hpp>
 #include <boost/test/unit_test.hpp>
-#include <iostream>
 #include <list>
 
 // Make all private members accessible for testing
@@ -10,29 +9,26 @@
 
 #include "../sep.hh"
 
-const auto testFileDir =
-    boost::dll::program_location().parent_path().parent_path() / "testfiles";
-
 /** Test cases for sep.hh */
 
-BOOST_AUTO_TEST_SUITE(SepPlugin_Tests)
+BOOST_AUTO_TEST_SUITE(Sep_Tests)
 
-BOOST_AUTO_TEST_CASE(sep_create) {
+BOOST_AUTO_TEST_CASE(create) {
   Sep::Ptr sep = Sep::create();
   BOOST_CHECK(sep != nullptr);
 }
 
-BOOST_AUTO_TEST_CASE(sep_getPluginName) {
+BOOST_AUTO_TEST_CASE(getPluginName) {
   Sep::Ptr sep = Sep::create();
   BOOST_CHECK(sep->getPluginName() == "SEP and SLI");
 }
 
-BOOST_AUTO_TEST_CASE(sep_getVersion) {
+BOOST_AUTO_TEST_CASE(getVersion) {
   Sep::Ptr sep = Sep::create();
   BOOST_CHECK(!sep->getPluginVersion().empty());
 }
 
-BOOST_AUTO_TEST_CASE(sep_filters_name) {
+BOOST_AUTO_TEST_CASE(filters_name) {
   Sep::Ptr sep = Sep::create();
   auto filters = sep->getFilters();
   BOOST_CHECK(filters.front() != nullptr);
