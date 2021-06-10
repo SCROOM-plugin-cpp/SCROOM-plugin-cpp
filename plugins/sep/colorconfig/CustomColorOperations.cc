0c18bd31b2d017baad2cfa0679ec0814f9b342bd
Jelle Bootsma <jellehb@gmail.com>
2021-06-10 11:35:16 +0200

    Implemented CCOperations, project builds

--- plugins/sep/colorconfig/CustomColorOperations.cc
+++ plugins/sep/colorconfig/CustomColorOperations.cc
@@ -3,6 +3,7 @@
 //
 
 #include <utility>
+#include <scroom/bitmap-helpers.hh>
 #include "CustomColorHelpers.hh"
 #include "CustomColorOperations.hh"
 
@@ -11,6 +12,45 @@ boost::shared_ptr<unsigned char> shared_malloc(size_t size)
     return boost::shared_ptr<unsigned char>(static_cast<unsigned char*>(malloc(size)), free);
 }
 
+PipetteLayerOperations::PipetteColor PipetteCommonOperationsCustomColor::sumPixelValues(Scroom::Utils::Rectangle<int> area,
+                                                                                 const ConstTile::Ptr          tile)
+{
+    int                                           offset = spp * (area.getTop() * tile->width + area.getLeft());
+    int                                           stride = spp * (tile->width - area.getWidth());
+    Scroom::Bitmap::SampleIterator<const uint8_t> si(tile->data.get(), 0, bps);
+    si += offset;
+
+    std::map<std::string, size_t> sums = {};
+
+    // Initialize sums of all colors to 0
+    for (auto color : colors){
+        sums[color->getName()] = 0;
+    }
+
+    for(int y = area.getTop(); y < area.getBottom(); y++)
+    {
+        for(int x = area.getLeft(); x < area.getRight(); x++)
+        {
+            for (auto color: colors){
+                sums[color->getName()] += *si++;
+            }
+        }
+        si += stride;
+    }
+
+    // Copy map to vector of pairs
+    PipetteColor result(sums.size());
+    copy(sums.begin(), sums.end(), result.begin());
+
+    return result;
+}
+
+
+
+
+OperationsCustomColors::OperationsCustomColors() : PipetteCommonOperationsCustomColor(8){}
+
+PipetteCommonOperationsCustomColor::Ptr OperationsCustomColors::create() { return PipetteCommonOperationsCustomColor::Ptr(new OperationsCustomColors()); }
 
 Scroom::Utils::Stuff OperationsCustomColors::cache(const ConstTile::Ptr tile) {
 
@@ -52,6 +92,63 @@ Scroom::Utils::Stuff OperationsCustomColors::cache(const ConstTile::Ptr tile) {
 
 
 }
+
+void OperationsCustomColors::reduce(Tile::Ptr target, const ConstTile::Ptr source, int top_left_x, int top_left_y) {
+    // Reducing by a factor 8
+    int         sourceStride = getBpp() * source->width / 8; // stride in bytes
+    const byte* sourceBase   = source->data.get();
+
+    int   targetStride = getBpp() * target->width / 8; // stride in bytes
+    byte* targetBase   = target->data.get() + (target->height * top_left_y + top_left_x) * targetStride / 8;
+
+    for(int y = 0; y < source->height / 8; y++)
+    {
+        byte* targetPtr = targetBase;
+
+        for(int x = 0; x < source->width / 8; x++)
+        {
+            // We want to store the average colour of the 8*8 pixel image
+            // with (x, y) as its top-left corner into targetPtr.
+            const byte* base = sourceBase + 8 * spp * x;  // start of the row
+            const byte* end  = base + 8 * sourceStride; // end of the row
+
+            std::map<std::string, size_t> sums = {};
+
+            // Initialize sums of all colors to 0
+            for (auto color : colors){
+                sums[color->getName()] = 0;
+            }
+
+
+            for(const byte* row = base; row < end; row += sourceStride) // Iterate over rows
+            {
+                for(size_t current = 0; current < 8 * spp; current += spp) // Iterate over pixels
+                {
+                    for (int i = 0; i < spp; i++){ // Iterate over samples in pixel
+                        sums[colors.at(i)->getName()] += row[current+i];
+                    }
+                }
+            }
+
+            // Calculate and store the average in the target
+            for (int i = 0; i < spp; i++) {
+                targetPtr[i] = static_cast<byte>(sums[colors.at(i)->getName()] / 64);
+            }
+
+
+            targetPtr += spp;
+        }
+
+        targetBase += targetStride;
+        sourceBase += sourceStride * 8;
+    }
+}
+
+int OperationsCustomColors::getBpp() {
+    return spp * bps;
+}
+
+
 
 
 void PipetteCommonOperationsCustomColor::setSpp(int samplesPerPixel) {
