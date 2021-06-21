6685322013db84a54aedf08dc13db95cfd06599a
Jelle Bootsma <jellehb@gmail.com>
2021-06-21 22:59:18 +0200

    Removed chrono

--- plugins/sep/colorconfig/CustomColorOperations.cc
+++ plugins/sep/colorconfig/CustomColorOperations.cc
@@ -56,7 +56,6 @@ OperationsCustomColors::create(int spp) {
 }
 
 Scroom::Utils::Stuff OperationsCustomColors::cache(const ConstTile::Ptr tile) {
-  auto start = std::chrono::high_resolution_clock::now();
   // Allocate the space for the cache - stride is the height of one row
   const int stride =
       cairo_format_stride_for_width(CAIRO_FORMAT_ARGB32, tile->width);
@@ -94,11 +93,7 @@ Scroom::Utils::Stuff OperationsCustomColors::cache(const ConstTile::Ptr tile) {
                                    // row, from the n channel source row
     row[target / 4] = 255u << 24 | R << 16 | G << 8 | B;
   }
-  auto end = std::chrono::high_resolution_clock::now();
 
-  auto duration =
-      std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-  std::cout << duration.count() / 1000 << std::endl;
   return Scroom::Bitmap::BitmapSurface::create(
       tile->width, tile->height, CAIRO_FORMAT_ARGB32, stride, data);
 }
