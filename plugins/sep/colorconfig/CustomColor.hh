b455163b72fa396b8e86bc08a36933d9a8cd64e1
Jelle Bootsma <jellehb@gmail.com>
2021-06-22 18:06:28 +0200

    cmykScaled introduction # Conflicts: #	CMakeLists.txt

--- plugins/sep/colorconfig/CustomColor.hh
+++ plugins/sep/colorconfig/CustomColor.hh
@@ -8,6 +8,7 @@
 #include <vector>
 
 #include <boost/shared_ptr.hpp>
+#include <cmath>
 
 class CustomColor {
 public:
@@ -19,11 +20,24 @@ public:
   double mMultiplier;
   double yMultiplier;
   double kMultiplier;
+
+  /*
+   * Scaled values are the same as the multipliers, but ints multiplied by 512
+   */
+  int16_t cScaled;
+  int16_t mScaled;
+  int16_t yScaled;
+  int16_t kScaled;
+
   CustomColor(std::string colorName, double c, double m, double y, double k) {
     name = std::move(colorName);
     cMultiplier = c;
     mMultiplier = m;
     yMultiplier = y;
     kMultiplier = k;
+    cScaled = static_cast<int16_t>(round(c * 256));
+    mScaled = static_cast<int16_t>(round(m * 256));
+    yScaled = static_cast<int16_t>(round(y * 256));
+    kScaled = static_cast<int16_t>(round(k * 256));
   }
 };
